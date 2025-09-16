7226de23bb1c88d65e9bb9c88eb9dd4e7a220c40
Zeiyar <marieycorentin97@gmail.com>
2025-09-16 12:04:44 +0200

    server name

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT||5000;
 const cors = require('cors');
 const mongoose = require('mongoose');
 const articlesRoutes = require('./routes/Articles');
-const authRoutes = require('./routes/auth');
+const authRoutes = require('./routes/Auth');
 
 const MONGO_URI = process.env.MONGO_URI;
 
