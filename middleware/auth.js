c5e79cb92c6b2cc22975087b0123a866e94f76b7
Zeiyar <marieycorentin97@gmail.com>
2025-09-18 13:20:30 +0200

    naaa

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,6 +4,9 @@ function auth (req,res,next){
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
 
+    res.setHeader("Access-Control-Allow-Origin", "https://reablog.netlify.app");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+
     if (!token) return res.status(401).json({message : "You need to connect before you do such a thing"});
 
     jwt.verify(token, process.env.JWT_SECRET, (err,decoded)=>{
--- server.js
+++ server.js
@@ -15,7 +15,10 @@ mongoose.connect(MONGO_URI, {
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
 
-app.use(cors());
+app.use(cors({
+  origin: "https://reablog.netlify.app",
+  credentials: true
+}));
 app.use(express.json());
 app.use('/articles', articlesRoutes);
 app.use('/auth', authRoutes);
